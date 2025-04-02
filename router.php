9ffed2a7f41eaa8d36ace5a3490f99d2021c5154
hamza <traorehamza407@gmail.com>
2025-04-02 09:22:47 +0200

    Ajout du formulaire de contact dans le template et mise à jour du routeur pour inclure la page de contact

---
+++ router.php
@@ -0,0 +1,32 @@
+<?php
+/** 
+* Le routeur est chargé d'écouter 
+* les requêtes HTTP provenent du client web
+* puis d'y répondre avec le contenu approprié
+*/
+
+//  if($_SERVER['REQUEST_URI'] === "/") {
+//     header("Location : https://google.fr");
+//  };
+
+switch ($_SERVER['REQUEST_URI']) {
+   case '/':
+      echo "Page d'accueil";
+      break;
+   case '/about':
+      echo "Page de presentation";
+      break;
+   case '/skills':
+      echo "Page de compétences";
+      break;
+   case '/projects':
+      echo "Page de projects";
+      break;
+   case '/contact':
+      include './template/contact.php';
+      break;
+   
+   default:
+      echo 'Page 404, introuvable';
+      break;
+}
