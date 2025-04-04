47a49b64e3d0f7c537fce1e1d4f14aaa5e8886e3
hamza <traorehamza407@gmail.com>
2025-04-04 13:17:17 +0200

    Ajout de la page projets avec mise en forme et contenu amélioré, intégration dans le routeur pour la gestion des routes

---
+++ templates/project.php
@@ -0,0 +1,30 @@
+<h1 class="bg-clip-text bg-gradient-to-br from-slate-800 to-slate-400 text-6xl font-bold text-transparent text-center">
+    Mes projets
+</h1>
+
+<div class="p-4 flex">
+    <div class="w-1/3 border-r border-slate-500">
+        <h3 class="text-2xl font-bold mb-5">Lorem ipsum</h3>
+        <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, voluptatem?</p>
+    </div>
+    <div class="w-screen border-r border-slate-500">
+        <h3 class="text-2xl font-bold mb-5">Stack</h3>
+        <ul class="flex gap-4">
+            <li class="">
+                <img src="https://placehold.co/40x40" alt="logo ">
+            </li>
+            <li class="">
+                <img src="https://placehold.co/40x40" alt="logo ">
+            </li>
+            <li class="">
+                <img src="https://placehold.co/40x40" alt="logo ">
+            </li>
+            <li class="">
+                <img src="https://placehold.co/40x40" alt="logo ">
+            </li>
+        </ul>
+    </div>
+    <div class="w-24 text-slate-400 flex items-center justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24"><path fill="currentColor" d="m8.006 21.308l-1.064-1.064L15.187 12L6.942 3.756l1.064-1.064L17.314 12z"/></svg>
+    </div>
+</div>
