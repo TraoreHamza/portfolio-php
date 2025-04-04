f2ea0e961513c90d867a11a644816fce81bf89fe
hamza <traorehamza407@gmail.com>
2025-04-04 11:44:00 +0200

    Ajout de la navigation mobile et des styles de base pour le header et le footer dans le template principal

--- templates/components/footer.php
+++ templates/components/footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-indigo-700 text-white fixed bottom-0 w-full">
-     <div class="p-2 text-center">
+     <div class="p-3 text-center">
          <p>© <?php echo date('Y'); ?> Ton prénom</p>
      </div>
  </footer>
