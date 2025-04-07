877d203ffe6bb9fad395598550ddb3f881b0b9be
hamza <traorehamza407@gmail.com>
2025-04-07 16:44:57 +0200

    Mise à jour du pied de page avec un nouveau style et ajustement du texte du sélecteur dans le formulaire pour une meilleure clarté

--- templates/components/footer.php
+++ templates/components/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-white p-2  border-t fixed bottom-0 w-full">
+<footer class="bg-white p-2 mt-10 border-t fixed bottom-0 w-full">
      <div class="p-3 text-center">
          <p>© <?php echo date('Y'); ?> Hamza</p>
      </div>
