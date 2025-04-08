e2f7a4230a4c2aaf831cfcbfa5693c8205f5c353
hamza <traorehamza407@gmail.com>
2025-04-08 16:25:07 +0200

    Ajout de la page de home et mise en page du structure

---
+++ templates/home.php
@@ -0,0 +1,23 @@
+<section class="grid grid-cols-8 grid-rows-10  h-screen w-full w-100">
+    <div class="flex col-span-3 row-span-5 col-start-4 row-start-1 border bg-[url(/image/home_2.png)] bg-cover relative">
+        <h2 class="text-3xl absolute bottom-0 right-0 text-white">Regarder mon travail </h2>
+    </div>
+    <div class="col-span-3 row-span-5 col-start-1 row-start-1 border bg-[url(/image/home_3.png)] bg-cover relative">
+        <h2 class="text-3xl absolute bottom-0 right-0 text-white ">Découvrez mon profil</h2>
+    </div>
+    <div class="col-span-3 row-span-5 col-start-1 row-start-6 border bg-[url(/image/home_3.png)] bg-cover relative">
+        <h2 class="text-3xl absolute bottom-0 right-0 text-white">Regarder mes compétences</h2>
+    </div>
+    <div class="col-span-2 row-span-3 col-start-7 row-start-1 border bg-[url(/image/home_1.png)] bg-cover relative">
+        <h2 class="text-3xl absolute bottom-0 right-0 text-white">Github</h2>
+    </div>
+    <div class="col-span-2 row-span-3 col-start-7 row-start-4 border bg-[url(/image/home_2.png)] bg-cover relative">
+        <h2 class="text-3xl absolute bottom-0 right-0 text-white">Linkedin</h2>
+    </div>
+    <div class="col-span-3 row-span-5 col-start-4 row-start-6 border bg-[url(/image/home_3.png)] bg-cover relative">
+        <h2 class="text-3xl absolute bottom-0 right-0 text-white">Contactez-moi</h2>
+    </div>
+    <div class="col-span-2 row-span-4 col-start-7 row-start-7 border bg-[url(/image/home_1.png)] bg-cover relative">
+        <h2 class="text-3xl absolute bottom-0 right-0 text-white">Instagram</h2>
+    </div>
+</section>
