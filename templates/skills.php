076bae2f479fde30b1d4d95bf444601827d5d6e0
hamza <traorehamza407@gmail.com>
2025-04-08 16:24:27 +0200

    Ajout de la page de compétance et mise en forme du structure

---
+++ templates/skills.php
@@ -0,0 +1,51 @@
+<div id="compétences" class="m-auto">
+        <div class="">
+            <h2>Découvrez mes compétences</h2>
+            <div class="grid grid-cols-3 gap-4">
+                <div class="">
+                    <img class="w-40" src="/image/html5.svg"  alt="">
+                    <h3>HTML</h3>
+                </div>
+                <div class="">
+                    <img class="w-40" src="/image/css.svg"  alt="">
+                    <h3>CSS</h3>
+                </div>
+                <div class="w25 skill">
+                    <img class="w-40" src="/image/javascript.svg" alt="">
+                    <h3>JS</h3>
+                </div>
+                <div class="w25 skill">
+                    <img class="w-40" src="/image/sass.svg"  alt="">
+                    <h3>PHP</h3>
+                </div>
+                <div class="w25 skill">
+                    <img class="w-40" src="/image/PHP-logo.svg.png"  alt="">
+                    <h3>PHP</h3>
+                </div>
+                <div class="w25 skill">
+                    <img class="w-40" src="/image/git (1).svg"  alt="">
+                    <h3>PHP</h3>
+                </div>
+                <div class="w25 skill">
+                    <img class="w-40" src="/image/github-light.svg"  alt="">
+                    <h3>GitHub</h3>
+                </div>
+                <div class="w25 skill">
+                    <img class="w-40" src="/image/nodejs.svg"  alt="">
+                    <h3>Node</h3>
+                </div>
+                <div class="w25 skill">
+                    <img class="w-40" src="/image/react_light.svg"  alt="">
+                    <h3>REACT</h3>
+                </div>
+                <div class="w25 skill">
+                    <img class="w-40" src="/image/swift.svg"  alt="">
+                    <h3>SWIFT</h3>
+                </div>
+                <div class="w25 skill">
+                    <img class="w-40" src="/image/figma (1).svg"  alt="">
+                    <h3>Figma</h3>
+                </div>
+            </div>
+        </div>
+    </div>
